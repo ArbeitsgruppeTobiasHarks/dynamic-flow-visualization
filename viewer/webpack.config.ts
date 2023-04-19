57ae1a2b0871f427283312fa4ee470fd07ba63d8
Michael Markl <michael.markl@uni-passau.de>
2023-04-19 11:39:01 +0200

    Work out packages

--- viewer/webpack.config.ts
+++ viewer/webpack.config.ts
@@ -1,50 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 const buildDir = path.resolve(__dirname, 'build')
 
 module.exports = {
-    entry: './src/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-        ],
-    },
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'main.[contenthash].js',
-        path: buildDir,
-        clean: true,
-    },
-    devServer: {
-        static: buildDir,
-        hot: true
-    },
-    optimization: {
-        minimizer: [
-            `...`,
-            new CssMinimizerPlugin(),
-        ]
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: path.resolve(__dirname, 'public'), to: buildDir }]
-        }),
-        new HtmlWebpackPlugin({ template: './public/index.html' }),
-        new MiniCssExtractPlugin()
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
     ]
-};
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'main.[contenthash].js',
+    path: buildDir,
+    clean: true
+  },
+  devServer: {
+    static: buildDir,
+    hot: true
+  },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()]
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: path.resolve(__dirname, 'public'), to: buildDir, globOptions: { ignore: ['**/index.html'] } }]
+    }),
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+    new MiniCssExtractPlugin()
+  ]
+}
